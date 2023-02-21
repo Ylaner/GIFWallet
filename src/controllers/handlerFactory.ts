d48de7eedff7e892d5883df28fa08c48e53a508e
Yasin Askarian <ylanerofficial@gmail.com>
2023-02-21 14:10:32 +0330

    first changes for search optimizations

--- src/controllers/handlerFactory.ts
+++ src/controllers/handlerFactory.ts
@@ -1,3 +1,5 @@
+import { Gif } from "../models/gifModel";
+
 export const sendMessage = async function (ctx: any, message: string) {
   await ctx.reply(message);
 };
@@ -10,3 +12,16 @@ export const wait = function (seconds: number) {
   });
 };
 //////////////////////////////
+export const searchForGIF = async function (ctx: any, index = null) {
+  const gifUniqueId = ctx.message?.animation?.file_unique_id!;
+  const user = ctx.user._id;
+  console.log(`gifId : ${gifUniqueId}`);
+  console.log(`userId : ${ctx.user._id}`);
+  const gif = await Gif.findOne({
+    gifUniqueId,
+    user,
+  });
+  console.log(gif);
+  if (gif) return gif;
+  else return undefined;
+};
