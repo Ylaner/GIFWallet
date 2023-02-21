e60b89ff76d18ecf6d96d0e49dd6d352b84d2388
Yasin Askarian <ylanerofficial@gmail.com>
2023-02-21 20:04:07 +0330

    back to normal

--- src/controllers/inlineQueriesControll.ts
+++ src/controllers/inlineQueriesControll.ts
@@ -1,7 +1,7 @@
 import { EvelateContext } from "../Types/Types";
 import { Gif } from "../models/gifModel";
 
-export const inlineQueriesControll = async function (ctx: EvelateContext) {
+exports.inlineQueriesControll = async function (ctx: EvelateContext) {
   console.log("inlineQueriesControll triggerd");
   const query = ctx.update.inline_query?.query!;
   // Test
@@ -15,7 +15,7 @@ export const inlineQueriesControll = async function (ctx: EvelateContext) {
       type: "gif",
       id: "GifWallet" + i,
       title: "Gif",
-      gif_file_id: gif.gifUniqueId,
+      gif_file_id: gif.gifId,
     };
   });
   await ctx.answerInlineQuery(replyGifs, {
