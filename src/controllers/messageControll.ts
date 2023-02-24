1f641ff32db18a298a14d404f796785c78110444
Yasin Askarian <ylanerofficial@gmail.com>
2023-02-24 13:37:04 +0330

    rebuild message router and  message controller

--- src/controllers/messageControll.ts
+++ src/controllers/messageControll.ts
@@ -1,38 +1,36 @@
 import { sendMessage } from "./handlerFactory";
 import { searchForGIF } from "./gifControll";
 import { GIFType } from "../Types/Types";
+//////////////////////////////////////////////////////////////////
 
-export const messageHandler = async (ctx: any) => {
-  console.log("message controll triggerd");
+//////////////////////////////////////////////////////////////////
+export const addKey = async (ctx: any) => {
+  console.log("addKey triggerd");
   const newUser = ctx.user;
-  switch (newUser.userOnStage?.stageName) {
-    case ctx.stageEnums.MESSAGE_PENDING:
-      const gif1 = await searchForGIF(newUser.userOnStage.details, newUser._id);
-      //Task 2 - Update gif.key
-      const keys1 = ctx.message.text.split(" ");
-      const newGif1: GIFType = gif1.toObject();
-      newGif1.key = keys1;
-      await gif1.updateOne(newGif1);
-      //Task 3 - Update user.userOnStage
-      newUser.userOnStage.stageName = ctx.stageEnums.GIF_SAVED;
-      newUser.userOnStage.details = null;
-      await ctx.user.updateOne(newUser);
-      //Task 4 - Send a message and done
-      await sendMessage(ctx, "Your GIF is saved.");
-      break;
-    case ctx.stageEnums.EDIT:
-      const gif2 = await searchForGIF(newUser.userOnStage.details, newUser._id);
-      const newGif2: GIFType = gif2.toObject();
-      const keys2 = ctx.message.text.split(" ");
-      newGif2.key = keys2;
-      await gif2.updateOne(newGif2);
-      sendMessage(ctx, "Your gif is edited");
-      newUser.userOnStage.stageName = ctx.stageEnums.GIF_SAVED;
-      newUser.userOnStage.details = null;
-      await ctx.user.updateOne(newUser);
-      break;
-    default:
-      await sendMessage(ctx, "Please send your GIF first.");
-      break;
-  }
+  const gif1 = await searchForGIF(newUser.userOnStage.details, newUser._id);
+  //Task 2 - Update gif.key
+  const keys1 = ctx.message.text.split(" ");
+  const newGif1: GIFType = gif1.toObject();
+  newGif1.key = keys1;
+  await gif1.updateOne(newGif1);
+  //Task 3 - Update user.userOnStage
+  newUser.userOnStage.stageName = ctx.stageEnums.GIF_SAVED;
+  newUser.userOnStage.details = null;
+  await ctx.user.updateOne(newUser);
+  //Task 4 - Send a message and done
+  await sendMessage(ctx, "Your GIF is saved.");
+};
+
+export const updateKey = async (ctx: any) => {
+  console.log("updateKey triggerd");
+  const newUser = ctx.user;
+  const gif = await searchForGIF(newUser.userOnStage.details, newUser._id);
+  const newGif: GIFType = gif.toObject();
+  const keys2 = ctx.message.text.split(" ");
+  newGif.key = keys2;
+  await gif.updateOne(newGif);
+  sendMessage(ctx, "Your gif is edited");
+  newUser.userOnStage.stageName = ctx.stageEnums.GIF_SAVED;
+  newUser.userOnStage.details = null;
+  await ctx.user.updateOne(newUser);
 };
--- src/routes/messageRouter.ts
+++ src/routes/messageRouter.ts
@@ -1,7 +1,23 @@
-import { messageHandler } from "../controllers/messageControll";
+import { sendMessage } from "../controllers/handlerFactory";
+import { addKey, updateKey } from "../controllers/messageControll";
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
 export const messageRouter = async function (ctx: any) {
   try {
-    await messageHandler(ctx);
+    console.log("message Router  triggerd");
+    const newUser = ctx.user;
+    switch (newUser.userOnStage?.stageName) {
+      case ctx.stageEnums.MESSAGE_PENDING:
+        await addKey(ctx);
+        break;
+      case ctx.stageEnums.EDIT:
+        await updateKey(ctx);
+        break;
+      default:
+        await sendMessage(ctx, "Please send your GIF first.");
+        break;
+    }
   } catch (err) {
     console.log(err);
   }
